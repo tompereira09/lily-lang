095ea490e1be4161402ab49698c15a058236c047
ParadoxV5 <paradox.ver5@gmail.com>
2023-02-17 16:12:15 -0700

    reimplement `Lexer#tokenize`

--- src/lexer.rb
+++ src/lexer.rb
@@ -1,38 +1,42 @@
+# frozen_string_literal: true
+
 class Lexer
   def initialize(string)
     @string = string
     @current_position = 0
-    @int_re = /\d+/
-    @tokens = ["(", ")", '"', " ", "+", "-", "*", "/"]
+    @re =
+      / (?<number>\d++)
+      | (?<word>\w++)
+      | (?<token>[()+\-*\/"])
+      | (?<linebreak>\R++)
+      | (?<whitespace>\s++)
+      | . # anything else
+      /x
     @keywords = ["printf", "eval"]
   end
 
   def tokenize
     actual_tokens = []
-    print_tokens = []
-    nums = []
-    curr_number = 0
-    data = @string.chars
-    for i in data
-      if i.match?(@int_re)
-        curr_number = @current_position
-        while curr_number < data.size and data[curr_number].match?(@int_re)
-          nums.append(data[curr_number])
-          curr_number += 1
+    @string.scan(@re) do
+      warn(
+        case
+          when i = $~[:number]
+            actual_tokens << i
+            "[NUMBER: #{i}]"
+          when i = $~[:word]
+            # TODO: @keywords
+          when i = $~[:token]
+            actual_tokens << i
+            "[TOKEN: #{i}]"
+          when $~[:linebreak]
+            # TODO: ?
+          when $~[:whitespace]
+            # TODO: ignore?
+        else
+          # TODO: raise error
         end
-        actual_tokens.append(nums.join(""))
-        print_tokens.append("[NUMBER: #{nums.join("")}]")
-        nums = []
-      end
-      for j in @tokens
-        if i == j
-          actual_tokens.append(i)
-          print_tokens.append("[TOKEN: #{i}]")
-        end
-      end
-      @current_position += 1
+      )
     end
-    puts print_tokens
-    return actual_tokens
+    actual_tokens
   end
 end
