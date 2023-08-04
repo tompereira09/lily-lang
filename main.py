6974b4c9cd253cf727bafd988197d63b352a2493
3faze <93428357+3faze@users.noreply.github.com>
2023-08-04 15:07:42 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import lexer #Temporary because it will be later passed to the parser
-
-file_path = sys.argv[1]
-
-lex_obj = lexer.Lexer()
-for i in lex_obj.tokenize(file_path):
-    print(i)
