f91cb2ac5633cffcc881a0cf969400b17372d2bf
3faze <93428357+3faze@users.noreply.github.com>
2023-08-19 18:46:03 +0100

    Add files via upload

--- translator.py
+++ translator.py
@@ -6,25 +6,18 @@ class Translator:
         output = open(filename[:-3]+".c", "w")
         output.write("#include <stdio.h>\n")
         output.write("int main() {\n")
-        output.write("\tint a;\n")
-        output.write("\tint b;\n")
-        output.write("\tint c;\n")
 
         for i in range(len(nodes)):
             self.curr_Node = nodes[i]
-            output.write(f'\ta = {self.curr_Node.left.token.value};\n')
-            output.write(f'\tb = {self.curr_Node.right.token.value};\n')
             match self.curr_Node.token.value:
                 case "+":
-                    output.write("\tc = a + b;\n")
+                    output.write(f'\tprintf("%d\\n", {self.curr_Node.left.token.value} + {self.curr_Node.right.token.value});\n')
                 case "-":
-                    output.write("\tc = a - b;\n")
+                    output.write(f'\tprintf("%d\\n", {self.curr_Node.left.token.value} - {self.curr_Node.right.token.value});\n')
                 case "*":
-                    output.write("\tc = a * b;\n")
+                    output.write(f'\tprintf("%d\\n", {self.curr_Node.left.token.value} * {self.curr_Node.right.token.value});\n')
                 case "/":
-                    output.write("\tc = a / b;\n")
-
-            output.write(f'\tprintf("%d\\n", c);\n')
+                    output.write(f'\tprintf("%d\\n", {self.curr_Node.left.token.value} / {self.curr_Node.right.token.value});\n')
 
         output.write("\treturn 0;\n")
         output.write("}\n")
