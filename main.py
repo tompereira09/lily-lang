30790d4e01081a9339be7dc80197ef004945a39b
3faze <93428357+3faze@users.noreply.github.com>
2023-08-21 15:52:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import lexer
 import astgen
 import translator
+import os
+
+to_parse = False
 
 file = input("filename: ")
+ans = input("ManualCompilation?(y/n)\n")
+print("\n\nOutput: ---------------------")
+if ans == "y":
+    to_parse = True
+elif ans == "n":
+    to_parse = False
+else:
+    to_parse = False
 tokenizer = lexer.Tokenizer()
 
 tokens = tokenizer.tokenize(file)
@@ -12,3 +23,9 @@ parsed = parser.parse(tokens)
 #	print(f'{i.left.token.value} {i.token.value} {i.right.token.value}')
 translator = translator.Translator()
 translated = translator.translate(parsed, file)
+
+if to_parse == True:
+    os.system(f'gcc {file[:-3]}.c')
+    os.system("./a.out")
+
+
